64a9137a07cd3caa0f929883c5fa8a93d5776249
angelynaliem <angelynaliem@gmail.com>
2018-03-18 00:27:40 -0400

    add Android App Button add Image Slider

---
+++ app/src/main/java/com/example/android/helloandroid/CustomSwipeAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.helloandroid;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by angelynaliem on 3/17/18.
+ */
+
+public class CustomSwipeAdapter extends PagerAdapter {
+
+    private int image_resources = (R.drawable.first, R.drawable.third, R.drawable.fourth);
+
+    private Context ctx;
+
+    private LayoutInflater layoutInflater;
+
+    public CustomSwipeAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public int getCount() {
+        return image_resources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view Object o) {
+        return (view==(LinearLayout)o);
+    }
+
+    @Override
+    public Object instantiateItem (ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View item_view = layoutInflater.inflate(R.layout.swipe_layout, container, false);
+        ImageView imageView = (ImageView) item_view.findViewById(R.id.image_view);
+        imageView.setImageResource(image_resources[position]);
+        container.addView(item_view);
+        return item_view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.android.helloandroid;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    CustomSwipeAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
     }
 
     public void goToWeb(View view) {
@@ -21,4 +28,12 @@ public class MainActivity extends AppCompatActivity {
         i.setData(Uri.parse(url));
         startActivity(i);
     }
+
+    public void goToAndroidApp (View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=com.udacity.android"));
+        startActivity(intent);
+    }
+
+
 }
